babb9012a312af1c633b95e062a06e4933f42c10
Domenico Commisso <dcommisso@users.noreply.github.com>
2024-10-12 12:41:31 +0200

    Add Source check to TestFileLoad

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -8,19 +8,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func TestStableFileLoad(t *testing.T) {
+func TestFileLoad(t *testing.T) {
 	cases := map[string]struct {
-		inputParams                    []string
-		expectedError                  string
-		expectedCertstoreElements      int
+		inputParams               []string
+		expectedError             string
+		expectedCertstoreElements int
+
 		expectedFirstCertificateSerial string
-		expectedLastCertificateSerial  string
+		expectedFirstCertificateSource string
+
+		expectedLastCertificateSerial string
+		expectedLastCertificateSource string
 	}{
 		"one big file": {
-			inputParams:                    []string{".internal/certs/testdata/tls-ca-bundle.pem"},
+			inputParams:                    []string{"../internal/certs/testdata/tls-ca-bundle.pem"},
 			expectedCertstoreElements:      142,
 			expectedFirstCertificateSerial: "5e:c3:b7:a6:43:7f:a4:e0",
-			expectedLastCertificateSerial:  "43:e3:71:13:d8:b3:59:14:5d:b7:ce:8c:fd:35:fd:6f:bc:05:8d:45",
+			expectedFirstCertificateSource: "../internal/certs/testdata/tls-ca-bundle.pem",
+
+			expectedLastCertificateSerial: "43:e3:71:13:d8:b3:59:14:5d:b7:ce:8c:fd:35:fd:6f:bc:05:8d:45",
+			expectedLastCertificateSource: "../internal/certs/testdata/tls-ca-bundle.pem",
 		},
 	}
 
@@ -51,7 +58,17 @@ func TestStableFileLoad(t *testing.T) {
 			lastCertificateIndex := len(config.certstore.Certs) - 1
 			gotLastCertificateSerial := config.certstore.Certs[lastCertificateIndex].GetSerialNumber()
 			if gotLastCertificateSerial != tc.expectedLastCertificateSerial {
-				t.Errorf("expected lastcertificate serial: %v - got: %v", tc.expectedLastCertificateSerial, gotLastCertificateSerial)
+				t.Errorf("expected last certificate serial: %v - got: %v", tc.expectedLastCertificateSerial, gotLastCertificateSerial)
+			}
+
+			gotFirstCertificateSource := config.certstore.Certs[0].Source
+			if gotFirstCertificateSource != tc.expectedFirstCertificateSource {
+				t.Errorf("expected first certificate source: %v - got: %v", tc.expectedFirstCertificateSource, gotFirstCertificateSource)
+			}
+
+			gotLastCertificateSource := config.certstore.Certs[lastCertificateIndex].Source
+			if gotLastCertificateSource != tc.expectedLastCertificateSource {
+				t.Errorf("expected last certificate source: %v - got: %v", tc.expectedLastCertificateSource, gotLastCertificateSource)
 			}
 		})
 	}
