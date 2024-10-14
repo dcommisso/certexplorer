ec021a285e08e8be6d55132399a3841664efbc04
Domenico Commisso <dcommisso@users.noreply.github.com>
2024-10-14 17:46:49 +0200

    Add formatter test

--- certformatter/formatter_test.go
+++ certformatter/formatter_test.go
@@ -106,6 +106,60 @@ func TestGetFormattedCertificate(t *testing.T) {
 				OutputFieldCertificateIndex: "0",
 			},
 		},
+		"all fields selected - none empty": {
+			inputCertIndex: 3,
+			expectedOutput: FormattedCertificate{
+				OutputFieldSubject: "Subject: CN=example.com",
+				OutputFieldIssuer:  "Issuer: CN=example.com",
+				OutputFieldSerialNumber: `Serial Number:
+    39:28:ed:76:45:6f:84:d0:77:9a:cb:0c:0f:e2:f4:d3:87:e5:b3:64`,
+				OutputFieldValidity: `Validity
+    Not Before: 2024-10-07 15:44:12 +0000 UTC
+    Not After : 2034-10-05 15:44:12 +0000 UTC`,
+				OutputFieldNotBefore: "Not Before: 2024-10-07 15:44:12 +0000 UTC",
+				OutputFieldNotAfter:  "Not After : 2034-10-05 15:44:12 +0000 UTC",
+				OutputFieldSKID: `Subject Key Identifier:
+    12:97:38:99:6E:64:A2:7E:CB:2F:57:7D:5B:E6:10:17:F7:2A:CA:55`,
+				OutputFieldAKID: `Authority Key Identifier:
+    12:97:38:99:6E:64:A2:7E:CB:2F:57:7D:5B:E6:10:17:F7:2A:CA:55`,
+				OutputFieldSANs: `Subject Alternative Name:
+    DNS:example.com, DNS:*.example.com, IP Address:10.0.0.1, IP Address:127.0.0.1`,
+				OutputFieldRawCert: `Raw Certificate:
+    -----BEGIN CERTIFICATE-----
+    MIIFQjCCAyqgAwIBAgIUOSjtdkVvhNB3mssMD+L004fls2QwDQYJKoZIhvcNAQEL
+    BQAwFjEUMBIGA1UEAwwLZXhhbXBsZS5jb20wHhcNMjQxMDA3MTU0NDEyWhcNMzQx
+    MDA1MTU0NDEyWjAWMRQwEgYDVQQDDAtleGFtcGxlLmNvbTCCAiIwDQYJKoZIhvcN
+    AQEBBQADggIPADCCAgoCggIBANuEMkqyS0UrlkAMKMzPsmvdwp0fefH564JXXOZ5
+    v5TnkubqI7ijKBXwSvPCTCuebZFYqIpqN58dSyeMewkEjngAH99+NAuIlt5nyAg5
+    +BxZiRNgugdnc6kAcBH+2C/3T4P0HSd0NPGVQOC4GxMAy6Shz5LiztunPkWPUxWy
+    8OZYO86xilW4L6Dv6JuPCsX7vzv90a31rtfghPlFUz4frcfGpdmTUeXv38/aVZRz
+    ToGgMvQUIaVvfxfmTqrWLCTy/4sTvIY8mVgCaGw5wNDv2oUiZ7C18w0orWeY0vc4
+    b+F+ma2Mwa04pj0KluwJFx85qH3lEk50yfnLYWUr3ZYwu2TOTqpDr61aR+NZqbw+
+    QZWrUImipTzbtVqezmzCLQqXI3zPsmhXb6+k/Ykj1xj9deE7vYAIc8MBVxhw1pYQ
+    fxRgx+dhyZpqYDW8fCUF/5vXtABm9we+/edL7vFKfBuSipeZeAie+a+SO6aMLCIZ
+    rMnH1PdJjzKTbCuWGA6K75y6X3vh2qenmh0/qVJljKktWfWq4Gp5u6EjcZ2mljzh
+    kDMMrRikWq/ThLOFLHC1wjy3IQ7SLGfnM9qharpHTVinwU1g97RGdzFXiB+y69O1
+    MlIlIXWeNJsU94pf1VidKz1hLJynw5cx7ifU+eSvbRU0ZLIHjuX3G4gVlwkUKPNN
+    rdgXAgMBAAGjgYcwgYQwHQYDVR0OBBYEFBKXOJluZKJ+yy9XfVvmEBf3KspVMB8G
+    A1UdIwQYMBaAFBKXOJluZKJ+yy9XfVvmEBf3KspVMA8GA1UdEwEB/wQFMAMBAf8w
+    MQYDVR0RBCowKIILZXhhbXBsZS5jb22CDSouZXhhbXBsZS5jb22HBAoAAAGHBH8A
+    AAEwDQYJKoZIhvcNAQELBQADggIBAEFfjgkurGI/ouVaJgCJYXmf+mJtexN6JYAB
+    XuCcobhUM+t5bfYt9DbZhaNC3pvtT9OijujajnXmd20QJLgXbWJe3qMbiDYroXZ/
+    ry8JHX9Nlp3wF3V/iGUQw4zLnFRalAeSyEAwg+nsoTwA6vkCompNFqzozh2ViSo+
+    ucWkb71Ky7Fl3HmKxp9ohG/0REwLTMYYmlPaGvk4o3oWAH3jm6g0fuFiw3mC1Gvr
+    nFiMva0JJ3LvaOZfoe2U2dSCFEleNqVMEvLNRF4Sd6SFR7/IhwjzvyxSw0DRSmmt
+    FZPakLw1Dm2eibT0rhEhn+7fLVHbaTSgOg8diXKqI+kqxbReYbrGkfa4lIfrvAMi
+    +ax9OQ54KIdQU3uqugyyTqqN1WERjvuv0uhWSF1sEk6AdpMWR8ymircQ72yEJmjI
+    ycRC2okZKkhYwFHb4nYwhQJOtzkmIUc90xmXZK4EvC9SYbHDu/8RBFRqkF4BnE8y
+    4eoywGFMRIJ4GdAzaI7rpQmtSvbZsiismBkTGU/IibM6udnnp5xD4R3HDDSdrkMH
+    MpupMy3sYHTJ+pi4OdpvYBfQHy1Cq0RxRc5SCSb0Dn6IggUVgyG0QpT4SuIM5qt8
+    3cp1JPJuZpbks3EkjqeyzKgfWsJWWi/9q68zbubW5oOgHk2548BO9x2njvEVfrTg
+    rDo5uzq8
+    -----END CERTIFICATE-----`,
+				OutputFieldSourceFile:       "From file: test",
+				OutputFieldCertificateIndex: "3",
+			},
+		},
 		"OutputFieldSourceFile as invalid selected field": {
 			inputCertIndex:      0,
 			inputSelectedFields: []Outputfield{OutputFieldCertificateIndex},
